eab9c9ad131dc21e652a8fd5ee4eb2be4418063f
KR <121792584+JKaliraj@users.noreply.github.com>
2023-06-19 18:47:39 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,4 @@
 echo " BUILD START"
-python3.9 -m pip install db-sqlite3 --user
 python3.9 -m pip install -r requirements.txt --user
 python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD END" 
